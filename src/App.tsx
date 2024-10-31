950199b7b169464515d07e1e87967f74e2944f96
Brunhilde <brandontu35@gmail.com>
2024-10-31 18:39:12 -0400

    feat started styling main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
 import netlifyIdentity from 'netlify-identity-widget';
+import { Button } from "@/components/ui/button"
+import { PlusIcon } from './components/plusicon';
+
 import './App.css'
+
+
+
 
 function App() {
   const navigate = useNavigate();
@@ -15,13 +21,24 @@ function App() {
   }, [loggedIn, navigate]);
 
   return (
-    <>
-      <h1>Snippey</h1>
+    <><div className='test bg-primary'>
+            <h1>Snippey</h1>
       <div className="card">
         <p>
           snippy bookmarks go here
         </p>
       </div>
+      <div className="fixed bottom-4 right-4 z-50">
+      <Button
+        size="icon"
+        className="h-14 w-14 rounded-full shadow-lg transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+      >
+        <PlusIcon className="h-14 w-14"/>
+        <span className="sr-only">Add</span>
+      </Button>
+    </div>
+    </div>
+
     </>
   )
 }
--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -9,7 +9,7 @@ export const Login = () => {
 
   const handleLogin = () => {
     if (netlifyIdentity && netlifyIdentity.currentUser()) {
-      netlifyIdentity.open();
+      navigate("/home");
     } else {
       netlifyIdentity.open();
       
@@ -19,7 +19,7 @@ export const Login = () => {
 
 
   return (
-    <div id="login" className="w-full relative bg-no-repeat bg-cover h-[500px] z-0 overflow-hidden bg-login">
+    <div id="login" className="w-full relative bg-no-repeat bg-cover h-[500px] z-0 overflow-hidden bg-login" >
       <div className="relative top-0 left-0 w-full h-full z-10 bg-black/50 flex items-center justify-center flex-col gap-5 pt-32">
         <h2 className="text-4xl font-bold font-manrope text-white">Snippey</h2>
         <h6 className="text-xl font-bold font-manrope text-white">A code snippet tool</h6>
