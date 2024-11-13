9118b1b0e39948236a715d8c91d074e8718ee5a4
Brunhilde <brandontu35@gmail.com>
2024-11-13 16:33:39 -0500

    feat more styling for sticky notes

--- src/components/stickynote.tsx
+++ src/components/stickynote.tsx
@@ -5,20 +5,25 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Textarea } from "@/components/ui/textarea"
 
+export const StickyNote = ({ title, body, tags }) => {
 
-export const StickyNote = ({title, body, tags}) => {
-
-    return <><Card>
-        <CardHeader>
-            <CardTitle>{title}</CardTitle>
-        </CardHeader>
-        <CardContent>
-            <p>{body}</p>
-        </CardContent>
-        <CardFooter>
-            <p>{tags}</p>
-        </CardFooter>
-    </Card></>
+    return <>
+        <Card className="bg-yellow-800 m-5 size-96">
+            <CardHeader>
+                <CardTitle className="text-neutral-950">{title}</CardTitle>
+            </CardHeader>
+            <CardContent className="w-full h-4/6" >
+                <Textarea className="h-60"placeholder={body} disabled />
+            </CardContent>
+            <CardFooter>
+                <Button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg></Button>
+                <Button variant="destructive"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg></Button>
+                <Button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg></Button>
+            </CardFooter>
+        </Card>
+    </>
 
 }
